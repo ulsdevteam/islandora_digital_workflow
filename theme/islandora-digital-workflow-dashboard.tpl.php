5bbfc6574e21f792235d6ad4d3d5ad52b571ab73
Brian Gillingham <bgilling@pitt.edu>
2018-06-14 15:25:23 -0400

    made bugfix to install file so that the sequence  field is a signed small integer, restored dashboard path handler menu route, allow model to be associated with more than one sequence

--- theme/islandora-digital-workflow-dashboard.tpl.php
+++ theme/islandora-digital-workflow-dashboard.tpl.php
@@ -12,7 +12,7 @@
 <div id="no-sidebars">
   <div class="dashboard-report">
     <h3>Links</h3>
-      <a href="/islandora/islandora_digital_workflow/create_batch">Create Batch</a>
+    <ul class="action-links"><li><a href="/islandora/islandora_digital_workflow/create_batch">Create Batch</a></li></ul>
     <ul>
     <?php foreach ($links as $link) { ?>
         <li><?php print $link; ?></li>
