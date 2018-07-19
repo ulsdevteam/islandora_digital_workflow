6bcdf688b75f195a6bc178f936c67b2ded7171a0
Brian Gillingham <bgilling@pitt.edu>
2018-07-19 13:37:59 -0400

    minor bugfixes after testing latest code as new install

--- theme/islandora-digital-workflow-dashboard-tasks.tpl.php
+++ theme/islandora-digital-workflow-dashboard-tasks.tpl.php
@@ -11,10 +11,14 @@
 ?>
 <div class="dashboard-tasks">
     <h3>The issues with the following batches need to be resolved:</h3>
+    <?php if (count($tasks_tables) > 0): ?>
     <?php foreach ($tasks_tables as $task_table): ?>
     <div class="dashboard-task">
       <?php print $task_table; ?>
     </div>
     <?php endforeach; ?>
+    <?php else: ?>
+    <em class="good">There are no batches that currently have any batches needing attention.  If there were, they would be listed here.</em>
+    <?php endif; ?>
 </div>
 <br style="clear:both" />
