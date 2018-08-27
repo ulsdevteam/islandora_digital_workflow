e082420502f6d16d25117179e175e4817e600701
Brian Gillingham <bgilling@pitt.edu>
2018-08-27 16:15:20 -0400

    added batch_completedness forena report

---
+++ forena_reports/sql/batch_completedness.sql
@@ -0,0 +1,31 @@
+-- ACCESS=access content
+-- REQUIRED ACTIONS BASED ON EACH BATCH
+SELECT wa.description `required action_id`, b.batch_name, b.nid, ws.name `workflow sequence name`
+FROM islandora_digital_workflow_batch b
+JOIN islandora_digital_workflow_sequence ws ON (ws.workflow_sequence_id = b.workflow_sequence_id)
+JOIN islandora_digital_workflow_sequence_actions wsa ON (wsa.workflow_sequence_id = ws.workflow_sequence_id)
+JOIN islandora_digital_workflow_actions wa ON (wa.action_id = wsa.action_id)
+WHERE 
+ wsa.action_id > 999 AND wsa.is_required = 1 AND wsa.is_system_action = 0 AND
+ wa.action_id NOT IN (
+            -- SET OF ACTUAL "BATCH" ACTIONS THAT HAVE BEEN COMPLETED FOR EACH BATCH
+            SELECT tx.action_id
+            FROM islandora_digital_workflow_transactions tx 
+            WHERE tx.batch_id = b.batch_id
+ )
+UNION 
+SELECT wa.description `required action_id`, b.batch_name, b.nid, ws.name `workflow sequence name`
+  FROM islandora_digital_workflow_batch_items bi 
+  JOIN islandora_digital_workflow_batch b ON (bi.batch_id = b.batch_id)
+  JOIN islandora_digital_workflow_sequence ws ON (ws.workflow_sequence_id = b.workflow_sequence_id)  
+  JOIN islandora_digital_workflow_sequence_actions wsa ON (wsa.workflow_sequence_id = ws.workflow_sequence_id)
+  JOIN islandora_digital_workflow_actions wa ON (wa.action_id = wsa.action_id)
+WHERE 
+ wsa.action_id < 1000 AND wsa.is_required = 1 AND wsa.is_system_action = 0 AND
+ wa.action_id NOT IN (
+            -- SET OF ACTUAL "BATCH_ITEM" ACTIONS THAT HAVE BEEN COMPLETED FOR EACH BATCH
+            SELECT tx.action_id
+            FROM islandora_digital_workflow_transactions tx
+            WHERE tx.batch_id = b.batch_id            
+        )
+GROUP BY bi.batch_id, wsa.action_id
