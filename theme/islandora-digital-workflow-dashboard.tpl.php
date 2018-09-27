8041b33f499d77eb55bba711166ff3c61c555854
Brian Gillingham <bgilling@pitt.edu>
2018-09-27 17:03:09 -0400

    added links on dashboard to various views of nodes that have each of the priority taxonomy values

--- theme/islandora-digital-workflow-dashboard.tpl.php
+++ theme/islandora-digital-workflow-dashboard.tpl.php
@@ -13,11 +13,16 @@
   <div class="dashboard-report">
     <ul class="action-links"><li><?php print implode("</li><li>", $action_links); ?></li></ul>
     <?php foreach ($links as $section => $section_links): ?>
+    <?php if ($section == "batch_stages"): ?><div class="batches-links"><?php endif; ?>
     <ul id="link_section_<?php print $section; ?>" class="<?php print $section; ?>-links">
+    <?php if ($section == "batch_stages"): ?><li class="list_header">by workflow stage</li><?php endif; ?>
+    <?php if ($section == "batch_priorities"): ?><li class="list_header">by priority</li><?php endif; ?>
+    <?php if ($section == "batch_content_types"): ?><li class="list_header">by content type</li><?php endif; ?>
       <?php foreach ($section_links as $link) { ?>
           <li><?php print $link; ?></li>
       <?php } ?>
     </ul>
+    <?php if ($section == "batch_content_types"): ?></div><?php endif; ?>
     <?php endforeach; ?>
     <br style="clear: both">
   </div><!-- /end dashboard-report -->
