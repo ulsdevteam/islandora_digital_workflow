5c8a182cdb58526851cf1d7e2d74ddf3edb07020
Brian Gillingham <bgilling@pitt.edu>
2018-10-02 12:26:41 -0400

    updated workflow node display for teaser to hide more fields, added report to dashboard display for batches that have been requested, rebranded the default batch values as 'Metadata defaults' and moved into the batch details area

--- theme/islandora-digital-workflow-dashboard.tpl.php
+++ theme/islandora-digital-workflow-dashboard.tpl.php
@@ -15,9 +15,9 @@
     <?php foreach ($links as $section => $section_links): ?>
     <?php if ($section == "batch_stages"): ?><div class="batches-links"><?php endif; ?>
     <ul id="link_section_<?php print $section; ?>" class="<?php print $section; ?>-links">
-    <?php if ($section == "batch_stages"): ?><li class="list_header">by workflow stage</li><?php endif; ?>
-    <?php if ($section == "batch_priorities"): ?><li class="list_header">by priority</li><?php endif; ?>
-    <?php if ($section == "batch_content_types"): ?><li class="list_header">by content type</li><?php endif; ?>
+    <?php if ($section == "batch_stages"): ?><li class="list_header">Filter by <b>Workflow Stage</b></li><?php endif; ?>
+    <?php if ($section == "batch_priorities"): ?><li class="list_header">Batches by <b>Priority</b></li><?php endif; ?>
+    <?php if ($section == "batch_content_types"): ?><li class="list_header">Batches by <b>Content Type</b></li><?php endif; ?>
       <?php foreach ($section_links as $link) { ?>
           <li><?php print $link; ?></li>
       <?php } ?>
