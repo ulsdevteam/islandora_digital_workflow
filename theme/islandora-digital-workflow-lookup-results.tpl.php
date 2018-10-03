b255e7bc35e2e91cfcffb17215bb865b0dd8f78e
Brian Gillingham <bgilling@pitt.edu>
2018-10-03 16:20:26 -0400

    improved search capabilities so that it searches with each method, and lists results according to how many reasons they matched on

--- theme/islandora-digital-workflow-lookup-results.tpl.php
+++ theme/islandora-digital-workflow-lookup-results.tpl.php
@@ -15,14 +15,12 @@
 <div class="lookup_results">
     <h3>Searched for "<?php print $searchterm; ?>"</h3>
     <p>Found <?php print $results_count; ?> results<?php
-    if ($matched_csv_only) : ?> <i>(found only in uploaded CSV file)</i><?php endif; ?><?php
-    if ($reason): ?>
-    <em>found in <b><?php print $reason; ?></b> values</em>
-    <?php endif; ?></p>
+    if ($matched_csv_only) : ?> <i>(found only in uploaded CSV file)</i><?php endif; ?></p>
 
     <?php $toggle = FALSE; ?>
     <?php foreach ($batch_records as $batch_record) { ?>
     <div class="lookup_result <?php print ($toggle) ? 'evenrow' : 'oddrow'; ?>">
+        <span class="small_lt_text">Match found in: <?php print implode(', ', $batch_record->reasons); ?></span>
         <?php
         $toggle = !$toggle;
         if ($batch_record->nid) {
@@ -34,7 +32,7 @@
         <div class="lookup_result_indent">
         <?php
         if (!($batch_record->nid)) : ?>
-          <h2><?php print l($batch_record->batch_name, '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record->batch_name); ?></h2
+          <h2><?php print l($batch_record->batch_name, '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record->batch_name); ?></h2>
         <?php endif; ?>
           <b>Description:</b> <?php print $batch_record->batch_description; ?>
           <?php if (isset($batch_record->identifiers)) { ?>
