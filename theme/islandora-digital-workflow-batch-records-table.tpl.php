d6cc60f4bdfaf82230fab22a751cad696d99365c
Brian Gillingham <bgilling@pitt.edu>
2018-10-15 12:27:23 -0400

    added a report for batches that have mismatches # of physical objects to # of items

--- theme/islandora-digital-workflow-batch-records-table.tpl.php
+++ theme/islandora-digital-workflow-batch-records-table.tpl.php
@@ -5,7 +5,10 @@
 * islandora-digital-workflow-batch-records-table.tpl display template.
 *
 * Variables available:
-* - $batch_records => array(),
+* - $batch_records => array() or stdClass, depending on the acutal routine that
+*                  found these batch records, there may be additional fields
+ *                 added to the stdClass object such as:
+ *                   $batch_record->node_field_object_count or $batch_record->is_batch_request
 * - $transaction_records => array(),
 * - $table_title => '',
 * - $table_description => '',
@@ -30,7 +33,11 @@
               <ul>
               <li><b>Priority:</b> <?php print $batch_record->priority; ?></li>
               <li><b>Description:</b> <?php print $batch_record->batch_description; ?></li>
-              <?php if (isset($batch_record->is_batch_request) && $batch_record->is_batch_request): ?>
+              <?php if (isset($batch_record->object_count) && isset($batch_record->field_pid_count_value) && $batch_record->object_count): ?>
+              <li><b>Actual items count:</b> <span class="bad"><?php print $batch_record->object_count; ?></span></li>
+              <li><b>Intended count:</b> <span class="bad"><?php print $batch_record->field_pid_count_value; ?></span></li>
+              <?php endif; ?>
+              <?php if (isset($batch_record->is_batch_request) && isset($batch_record->how_long_from_now) && $batch_record->is_batch_request): ?>
               <li><b>Requestor:</b> <?php print $batch_record->batch_requestor; ?></li>
               <li><b>Request due date:</b> <span class="bad"><?php print format_date($batch_record->batch_request_due_date, 'custom', 'Y/m/d'); ?></span>
                   <i>- <?php print $batch_record->how_long_from_now; ?></i></li>
