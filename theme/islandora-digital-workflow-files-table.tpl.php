63f5590344830590cee320c65b80f5aea9d1b87e
Brian Gillingham <bgilling@pitt.edu>
2018-11-07 11:26:48 -0500

    minor UI adjustment to the dashboard display of problems folders

--- theme/islandora-digital-workflow-files-table.tpl.php
+++ theme/islandora-digital-workflow-files-table.tpl.php
@@ -45,9 +45,6 @@
           </div>
       </div>
       <?php } ?>
-      <?php if (!$action_link): ?>
-      <em>Mouse-over any of the "problem_notes.txt" to see what needs to be done to resolve each problem.</em><hr>
-      <?php endif; ?>
       <?php if ($root): ?><span class="small_lt_text">Folders and files located under: <em><?php print $root; ?></em></span><?php endif; ?>
     </div>
 </div>
