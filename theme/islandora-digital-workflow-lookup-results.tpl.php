f54edcaf2702cebccf878dd0806ef5dfa676a902
Brian Gillingham <bgilling@pitt.edu>
2018-11-30 17:08:20 -0500

    ISLANDORA-240 moved the configuration options for digitazation request searching and mapping into the module settings page and coded out the dynamic search

--- theme/islandora-digital-workflow-lookup-results.tpl.php
+++ theme/islandora-digital-workflow-lookup-results.tpl.php
@@ -6,7 +6,7 @@
 *
 * Variables available:
 * - $searchterm = string
-* - $batch_records => array(),
+* - $records => array(),
 * - $results_count => integer
 * - $matched_csv_only => boolean
 *
@@ -17,28 +17,40 @@
     <p>Found <?php print $results_count; ?> results<?php
     if ($matched_csv_only) : ?> <i>(found only in uploaded CSV file)</i><?php endif; ?></p>
 
+    <?php $digital_request_breaker_displayed = FALSE; ?>
     <?php $toggle = FALSE; ?>
-    <?php foreach ($batch_records as $batch_record) { ?>
+    <?php foreach ($records as $record) { ?>
+<?php dpm($record); ?>
+
+        <?php if (!$digital_request_breaker_displayed && (!(array_search('digitization requests', $record->reasons) === FALSE))): ?>
+          <h3>Digitization Requests</h3>
+          <?php $digital_request_breaker_displayed = TRUE; ?>
+        <?php endif; ?>
+
     <div class="lookup_result <?php print ($toggle) ? 'evenrow' : 'oddrow'; ?>">
-        <span class="small_lt_text">Match found in: <?php print implode(', ', $batch_record->reasons); ?></span>
         <?php
         $toggle = !$toggle;
-        if ($batch_record->nid) {
-          $node = node_load($batch_record->nid);
+        if ($record->nid && (array_search('digitization requests', $record->reasons) === FALSE)) {
+          $node = node_load($record->nid);
           $view = node_view($node, 'teaser');
           print drupal_render($view);
         }
         ?>
         <div class="lookup_result_indent">
-        <?php
-        if (!($batch_record->nid)) : ?>
-          <h2><?php print l($batch_record->batch_name, '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record->batch_name); ?></h2>
-        <?php endif; ?>
-          <b>Description:</b> <?php print $batch_record->batch_description; ?>
-          <?php if (isset($batch_record->identifiers)) { ?>
-          <div><b>Matched Identifiers:</b> <?php print $batch_record->identifiers; ?></div>
+        <?php if (!(array_search('digitization requests', $record->reasons) === FALSE)) : ?>
+          <b>Request data:</b> <?php print l($record->data, '/node/' . $record->nid . '/submission/' . $record->sid); ?>
+        <?php else: ?>
+          <?php if (!($record->nid)) : ?>
+            <h2><?php print l($record->batch_name, '/islandora/islandora_digital_workflow/edit_batch/' . $record->batch_name); ?></h2>
+          <?php endif; ?>
+          <b>Description:</b> <?php print $record->batch_description; ?>
+          <?php if (isset($record->identifiers)) { ?>
+          <div><b>Matched Identifiers:</b> <?php print $record->identifiers; ?></div>
           <?php } ?>
+          <?php endif; ?>
         </div>
+        <span class="small_lt_text">Match found in: <?php print implode(', ', $record->reasons); ?></span>
     </div>
     <?php } ?>
 <br style="clear:both" />
+</div>
