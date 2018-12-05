baca061d293c7f3af4e79fa2bf969401a8b96ad4
Brian Gillingham <bgilling@pitt.edu>
2018-12-05 16:50:55 -0500

    ISLANDORA-240 added HTML paragraphs above any digitization request submission page, added a configuration option for each of the digitization request types to inject that HTML at the head of submissions to that webform

--- theme/islandora-digital-workflow-batch-records-table.tpl.php
+++ theme/islandora-digital-workflow-batch-records-table.tpl.php
@@ -28,24 +28,28 @@
           <?php
           $toggle = !$toggle;
           ?>
-          <div class="lookup_result_indent">
-              <h3><a href="<?php print ($batch_record->nid) ? '/node/' . $batch_record->nid . '/batch' : '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record->batch_name; ?>"><?php print $batch_record->batch_name; ?></a></h3>
-              <ul>
-              <li><b>Description:</b> <?php print nl2br($batch_record->batch_description); ?></li>
-              <?php if (isset($batch_record->priority) && $batch_record->priority): ?>
-              <li><b>Priority:</b> <?php print $batch_record->priority; ?></li>
-              <?php endif; ?>
-              <?php if (isset($batch_record->object_count) && isset($batch_record->items_count)): ?>
-              <li><b>Actual items count:</b> <span class="bad"><?php print $batch_record->object_count; ?></span></li>
-              <li><b>Intended count:</b> <span class="bad"><?php print $batch_record->items_count; ?></span></li>
-              <?php endif; ?>
-              <?php if (isset($batch_record->is_batch_request) && isset($batch_record->how_long_from_now) && $batch_record->is_batch_request): ?>
-              <li><b>Requestor:</b> <?php print $batch_record->batch_requestor; ?></li>
-              <li><b>Request due date:</b> <span class="bad"><?php print format_date($batch_record->batch_request_due_date, 'custom', 'Y/m/d'); ?></span>
-                  <i>- <?php print $batch_record->how_long_from_now; ?></i></li>
-              <?php endif; ?>
-              </ul>
-          </div>
+          <fieldset class="collapsible collapsed straight-edge" id="box_<?php print $batch_record->batch_id; ?>_<?php print $table_title; ?>" >
+          <!-- <div class="lookup_result_indent"> -->
+              <legend><span class="fieldset-legend"><?php print $batch_record->batch_name; ?></span></legend>
+              <div class="fieldset-wrapper" style="display: none;">
+                <h3><a href="<?php print ($batch_record->nid) ? '/node/' . $batch_record->nid . '/batch' : '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record->batch_name; ?>"><?php print $batch_record->batch_name; ?></a></h3>
+                <ul class="indented_ul">
+                <li><b>Description:</b> <?php print nl2br($batch_record->batch_description); ?></li>
+                <?php if (isset($batch_record->priority) && $batch_record->priority): ?>
+                <li><b>Priority:</b> <?php print $batch_record->priority; ?></li>
+                <?php endif; ?>
+                <?php if (isset($batch_record->object_count) && isset($batch_record->items_count)): ?>
+                <li><b>Actual items count:</b> <span class="bad"><?php print $batch_record->object_count; ?></span></li>
+                <li><b>Intended count:</b> <span class="bad"><?php print $batch_record->items_count; ?></span></li>
+                <?php endif; ?>
+                <?php if (isset($batch_record->is_batch_request) && isset($batch_record->how_long_from_now) && $batch_record->is_batch_request): ?>
+                <li><b>Requestor:</b> <?php print $batch_record->batch_requestor; ?></li>
+                <li><b>Request due date:</b> <span class="bad"><?php print format_date($batch_record->batch_request_due_date, 'custom', 'Y/m/d'); ?></span>
+                    <i>- <?php print $batch_record->how_long_from_now; ?></i></li>
+                <?php endif; ?>
+                </ul>
+              </div>
+          </fieldset>
       </div>
       <?php } ?>
     </div>
