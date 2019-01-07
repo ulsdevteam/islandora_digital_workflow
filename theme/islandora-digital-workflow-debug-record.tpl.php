c5e1888f3d2441ba19c47bae8314dc9f0cf46304
Willow Gillingham <bgilling@pitt.edu>
2019-01-07 16:13:03 -0500

    touchup to correct display of drush calls log output

--- theme/islandora-digital-workflow-debug-record.tpl.php
+++ theme/islandora-digital-workflow-debug-record.tpl.php
@@ -11,14 +11,15 @@
 <?php $output_saved_for_bottom = ''; ?>
 <?php foreach ($debug_record as $fieldname => $value): ?>
 <?php if ($fieldname == 'output') :
-  $output_saved_for_bottom = $value;
+  // This $value is a string that becomes an array when unserialized
+  $output_saved_for_bottom = implode("\n", unserialize($value));
 else: ?>
 <div>
     <label class="fixed_label_width"><?php print $fieldname; ?></label><?php print $value; ?>
 </div>
 <?php endif; ?>
 <?php endforeach; ?>
-<?php if ($output_saved_for_bottom): ?>
-  <div><label class="fixed_label_width"><?php print $fieldname; ?></label></div>
-  <textarea readonly class="as_terminal_output textarea_terminal"><?php print unserialize($output_saved_for_bottom); ?></textarea>
+<?php if ($output_saved_for_bottom <> ''): ?>
+  <div><label class="fixed_label_width">call output</label></div>
+  <textarea readonly class="as_terminal_output textarea_terminal"><?php print $output_saved_for_bottom; ?></textarea>
 <?php endif; ?>
