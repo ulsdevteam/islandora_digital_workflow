68a1a5d6bd9c801ddbbace2a78f54d754167cbd9
Willow Gillingham <bgilling@pitt.edu>
2019-01-29 12:39:35 -0500

    ISLANDORA-245 added some links between a batch and a digitization request.  also any digitization request that has already been mapped will display a different message to notify users that the request has already been mapped at least once

--- theme/islandora-digital-workflow-batch-description.tpl.php
+++ theme/islandora-digital-workflow-batch-description.tpl.php
@@ -16,5 +16,8 @@
     <label>Batch name:</label> <a href="/node/<?php print $batch_record['nid']; ?>/batch"><?php print $batch_record['batch_name']; ?></a><br >
     <label>Progress:</label> <span class="progress_<?php print strtolower(str_replace(" ", "_", $batch_record['progress'])); ?>"><?php print $batch_record['progress']; ?></span><br>
     <label>Batch description:</label> <?php print $batch_record['batch_description']; ?>
+    <?php if ($batch_mapped_from_webformsubmission) : ?>
+    <br><label>Submission: </label> <a target="_blank" href="/node/<?php print $batch_mapped_from_webformsubmission->nid; ?>/submission/<?php print $batch_mapped_from_webformsubmission->sid; ?>">"<?php print $batch_mapped_from_webformsubmission->title; ?>" #<?php print $batch_mapped_from_webformsubmission->sid; ?></a>
+    <?php endif; ?>
 </div>
 <?php endif; ?>
