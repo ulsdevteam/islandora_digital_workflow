a507189baf791ec5698dfdff0ee9852d81f524da
Willow Gillingham <bgilling@pitt.edu>
2019-01-31 17:28:53 -0500

    ISLANDORA-245 added configuration options to define mapping for any webform multiple-value fields... values are not used yet in the form that creates batches from digitization request

--- js/admin_digital_workflow.js
+++ js/admin_digital_workflow.js
@@ -9,3 +9,23 @@ jQuery( document ).ready(function() {
     jQuery(".even_child").parents('div .form-item').addClass("even_element");        
 });
 
+function model_mapping_selected(control, form_variables) {
+    if (control.value) {
+        var fieldset_id = 'edit-webformfield-fieldset-' + form_variables[control.value]['nid'] + '-' + control.value;
+        var fieldset = document.getElementById(fieldset_id);
+        var element_name = 'model_mappings_' + form_variables[control.value]['nid'];
+        var element = document.getElementById(element_name);
+        // If the item is a select element
+        if (form_variables[control.value]['type'] == 'select') {
+            fieldset.style.display = "block";
+//            var extra = JSON.parse(JSON.stringify(form_variables[control.value].extra));
+//            console.log(extra);
+//            console.log(extra.items);
+//            var items_obj = extra.items;
+//            console.log(items_obj);
+        }
+        else {
+            fieldset.style.display = "none";
+        }
+    }
+}
