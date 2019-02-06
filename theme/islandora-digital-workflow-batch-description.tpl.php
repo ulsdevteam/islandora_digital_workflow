40b075090e3e39b870a28c0f29151ac66d1f91c0
Willow Gillingham <bgilling@pitt.edu>
2019-02-06 17:34:16 -0500

    added a class for links that open in a _blank target

--- theme/islandora-digital-workflow-batch-description.tpl.php
+++ theme/islandora-digital-workflow-batch-description.tpl.php
@@ -11,13 +11,13 @@
  * - $batch_record
  */
 ?>
-<?php if ($batch_record && array_key_exists('nid', $batch_record) && $batch_record['nid']): ?>
+<?php if (is_array($batch_record)): ?>
 <div class="lookup_result batch-description">
-    <label>Batch name:</label> <a href="/node/<?php print $batch_record['nid']; ?>/batch"><?php print $batch_record['batch_name']; ?></a><br >
+    <label>Batch name:</label> <a href="<?php print ($batch_record['nid']) ? '/node/' . $batch_record['nid'] . '/batch' : '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record['batch_name']; ?>"><?php print $batch_record['batch_name']; ?></a><br >
     <label>Progress:</label> <span class="progress_<?php print strtolower(str_replace(" ", "_", $batch_record['progress'])); ?>"><?php print $batch_record['progress']; ?></span><br>
     <label>Batch description:</label> <?php print $batch_record['batch_description']; ?>
     <?php if ($batch_mapped_from_webformsubmission) : ?>
-    <br><label>Submission: </label> <a target="_blank" href="/node/<?php print $batch_mapped_from_webformsubmission->nid; ?>/submission/<?php print $batch_mapped_from_webformsubmission->sid; ?>">"<?php print $batch_mapped_from_webformsubmission->title; ?>" #<?php print $batch_mapped_from_webformsubmission->sid; ?></a>
+    <br><label>Submission: </label> <a class="link_open_new_tab" title="link opens in separate tab" target="_blank" href="/node/<?php print $batch_mapped_from_webformsubmission->nid; ?>/submission/<?php print $batch_mapped_from_webformsubmission->sid; ?>">"<?php print $batch_mapped_from_webformsubmission->title; ?>" #<?php print $batch_mapped_from_webformsubmission->sid; ?></a>
     <?php endif; ?>
 </div>
 <?php endif; ?>
