499c9eebc029429c5fb1186724bf99ecabc424b0
Willow Gillingham <bgilling@pitt.edu>
2019-02-25 17:03:47 -0500

    ISLANDORA-250 ISLANDORA-252 - added function handlers for publish all and ingest all - 'do_publish_item' pending completed development

--- theme/islandora-digital-workflow-batch-description.tpl.php
+++ theme/islandora-digital-workflow-batch-description.tpl.php
@@ -18,14 +18,16 @@
 <div class="lookup_result batch-description">
     <label>Batch name:</label> <a href="<?php print ($batch_record['nid']) ? '/node/' . $batch_record['nid'] . '/batch' : '/islandora/islandora_digital_workflow/edit_batch/' . $batch_record['batch_name']; ?>"><?php print $batch_record['batch_name']; ?></a><br >
     <label>Batch progress:</label>
+    <div class="progress_div">
           <?php foreach (array('New', 'In Progress', 'Prepared', 'Ingested', 'Completed') as $progress_step) : ?>
           <span class="progress_<?php print (($batch_record['progress'] == $progress_step) ?
             strtolower(str_replace(" ", "_", $batch_record['progress'])) : "na"); ?>"><?php print $progress_step; ?></span>
-          <?php print (($progress_step <> 'Completed') ? ' | ' : ''); ?>
+          <?php print (($progress_step <> 'Completed') ? ' &rsaquo; ' : ''); ?>
       <?php endforeach; ?>
     <?php if ($can_ingest_all): ?> | <a href="#">INGEST ALL ITEMS</a><?php endif; ?>
     <?php if ($can_publish_all): ?> | <a href="#">PUBLISH ALL ITEMS</a><?php endif; ?>
-    <br>
+    </div>
+    <br class="break_float">
     <label>Batch description:</label> <?php print $batch_record['batch_description']; ?>
     <?php if ($batch_mapped_from_webformsubmission) : ?>
     <br><label>Submission: </label> <a class="link_open_new_tab" title="link opens in separate tab" target="_blank" href="/node/<?php print $batch_mapped_from_webformsubmission->nid; ?>/submission/<?php print $batch_mapped_from_webformsubmission->sid; ?>">"<?php print $batch_mapped_from_webformsubmission->title; ?>" #<?php print $batch_mapped_from_webformsubmission->sid; ?></a>
