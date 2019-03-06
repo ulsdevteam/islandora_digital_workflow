dac6ee27b3709a73ab800ec5c24fca1faf93e720
Willow Gillingham <bgilling@pitt.edu>
2019-03-06 15:39:33 -0500

    UI improvements to glyphs and some CSS and markup

--- theme/islandora-digital-workflow-admin-workflow-sequence.tpl.php
+++ theme/islandora-digital-workflow-admin-workflow-sequence.tpl.php
@@ -27,7 +27,7 @@
       <?php endif; ?>
     </div>
     <h3><a name="<?php print $sequence_name; ?>"><?php print l($sequence_name, 'admin/islandora/islandora_digital_workflow/edit_workflow_sequence/' . $workflow_sequence_id); ?></a></h3>
-    <p class="small_lt_text"><?php print $sequence_description; ?></p>
+    <p class="small_text"><b>Sequence description:</b> <?php print $sequence_description; ?></p>
     <div class="lookup_result_square">
       <p><?php print l(t('Edit Models'), 'admin/islandora/islandora_digital_workflow/workflow_sequence_models/' . $workflow_sequence_id, $link_button_class); ?> <?php if ($models) { print 'Models: ' . $models; } ?></p>
       <?php if ($is_mixed): ?>
@@ -37,18 +37,20 @@
         <?php if ((array_key_exists($model, $markup_batch_actions) && count($markup_batch_actions[$model]) > 0) || (array_key_exists($model, $markup_item_actions) && count($markup_item_actions[$model]) > 0)): ?>
           <ul>
           <?php if (array_key_exists($model, $markup_batch_actions) && count($markup_batch_actions[$model]) > 0): ?>
-            <li class="no_indent">Batch actions
+            <li class="no_indent"><b>Batch actions</b>
               <ul class="batch_action_box corner_bordered"><?php print implode("\n" , $markup_batch_actions[$model]); ?></ul></li>
           <?php endif; ?>
           <?php if (array_key_exists($model, $markup_item_actions) && count($markup_item_actions[$model]) > 0): ?>
-            <li class="no_indent">Item actions
+            <li class="no_indent"><b>Item actions</b>
               <ul class="corner_bordered item_action_box"><?php print implode("\n" , $markup_item_actions[$model]); ?></ul></li>
           <?php endif; ?>
           </ul>
         <?php endif; ?>
         <?php endforeach; ?>
           <?php if ($sequence_last_modified): ?>
-          <li><div class="admin_links disabled_text">Sequence last modified: <b><?php print (($sequence_last_modified == '0000-00-00 00:00:00') ? '(not set)' : $sequence_last_modified); ?></b></div></li>
+          <li><div class="admin_links disabled_text">
+                  Sequence last modified: <b><?php print (($sequence_last_modified == '0000-00-00 00:00:00') ? '(not set)' : $sequence_last_modified); ?></b>
+              </div></li>
           <?php endif; ?>
         </ul>
       <?php else: ?>
@@ -56,11 +58,11 @@
         <?php if ((count($markup_batch_actions) > 0) || (count($markup_item_actions) > 0)): ?>
         <ul>
         <?php if (count($markup_batch_actions) > 0): ?>
-          <li class="no_indent">Batch actions
+          <li class="no_indent"><b>Batch actions</b>
             <ul class="batch_action_box corner_bordered"><?php print implode("\n" , $markup_batch_actions); ?></ul></li>
         <?php endif; ?>
         <?php if (count($markup_item_actions) > 0): ?>
-          <li class="no_indent">Item actions
+          <li class="no_indent"><b>Item actions</b>
             <ul class="corner_bordered item_action_box"><?php print implode("\n" , $markup_item_actions); ?></ul></li>
         <?php endif; ?>
           <?php if ($sequence_last_modified): ?>
