c734ec53bbafa62388ffd0af0a832e64e00d6a68
Willow Gillingham <bgilling@pitt.edu>
2019-03-11 14:58:44 -0400

    updated reports to display the ingested items - and filter out any batches that are completed

--- forena_reports/sql/batch_completedness.sql
+++ forena_reports/sql/batch_completedness.sql
@@ -6,7 +6,8 @@ JOIN islandora_digital_workflow_sequence ws ON (ws.workflow_sequence_id = b.work
 JOIN islandora_digital_workflow_sequence_actions wsa ON (wsa.workflow_sequence_id = ws.workflow_sequence_id)
 JOIN islandora_digital_workflow_actions wa ON (wa.action_id = wsa.action_id)
 WHERE 
- wsa.is_required = 1 AND wsa.is_ingest_prerequisite = 0 AND wa.is_batch_action = 1 AND
+ b.progress <> 'Completed' AND
+ wsa.is_publish_prerequisite = 1 AND wsa.is_ingest_prerequisite = 0 AND wa.is_batch_action = 1 AND
  wa.action_id NOT IN (
             -- SET OF ACTUAL "BATCH" ACTIONS THAT HAVE BEEN COMPLETED FOR EACH BATCH
             SELECT tx.action_id
@@ -21,7 +22,8 @@ SELECT wa.description `required action_id`, b.batch_name, b.nid, ws.name `workfl
   JOIN islandora_digital_workflow_sequence_actions wsa ON (wsa.workflow_sequence_id = ws.workflow_sequence_id)
   JOIN islandora_digital_workflow_actions wa ON (wa.action_id = wsa.action_id)
 WHERE 
- wsa.is_required = 1 AND wsa.is_ingest_prerequisite = 0 AND wa.is_batch_action = 0 AND
+ b.progress <> 'Completed' AND 
+ wsa.is_publish_prerequisite = 1 AND wsa.is_ingest_prerequisite = 0 AND wa.is_batch_action = 0 AND
  wa.action_id NOT IN (
             -- SET OF ACTUAL "BATCH_ITEM" ACTIONS THAT HAVE BEEN COMPLETED FOR EACH BATCH
             SELECT tx.action_id
