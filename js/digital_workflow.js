c451f2704441be0926186ca68337fd3b64cca726
Willow Gillingham <bgilling@pitt.edu>
2019-05-14 16:38:10 -0400

    ISLANDORA-270 ISLANDORA-264 added  for metadata review action, adjusted item display to show files for paged content, and a bunch of other little details that were more obvious when installing for first time on an installation

--- js/digital_workflow.js
+++ js/digital_workflow.js
@@ -7,14 +7,15 @@
 /*
  * This will hide / show the "notes" form element depending on whether or not 
  * the current selection is an action that requires notes.  Currently, this is
- * only for IDW_ACTION_PROBLEM.
+ * only for IDW_ACTION_PROBLEM and IDW_ACTION_METADATA_FAIL_QC.
  * 
  * @param {type} control
  * @returns {undefined}
  */
 function add_action_selection(control) {
   // IDW_ACTION_PROBLEM == 9
-  if (control.value == 9) {
+  // IDW_ACTION_METADATA_FAIL_QC = 13
+  if (control.value == 9 || control.value == 13) {
     jQuery('#edit-notes-wrapper').show();
     jQuery('#edit-notes-wrapper').removeClass("display_none");
   }
